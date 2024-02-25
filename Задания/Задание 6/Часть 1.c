f51ed02b5e49267a54f90952c6616c59b19801b6
Lebovsky0 <lebovsky001@gmail.com>
2024-02-25 19:39:06 +0700

    Update Часть 1.c

--- Задания/Задание 6/Часть 1.c
+++ Задания/Задание 6/Часть 1.c
@@ -1,140 +1,155 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+
+#define NAME_LENGTH 10
+#define TEL_LENGTH 10
 
 struct abonent {
-    char name[10];
-    char second_name[10];
-    char tel[10];
+    char name[NAME_LENGTH];
+    char second_name[NAME_LENGTH];
+    char tel[TEL_LENGTH];
 };
 
-void add_abonent(struct abonent** directory, int* size) {
-    *size += 1;
-    *directory = reinterpret_cast<abonent*>(realloc(*directory, (*size) * sizeof(struct abonent)));
-    
-    struct abonent new_abonent;
+// Функция для добавления абонента в справочник
+void addAbonent(struct abonent **abonents, int *count) {
+    struct abonent newAbonent;
+
     printf("Введите имя абонента: ");
-    scanf("%s", new_abonent.name);
+    scanf("%s", newAbonent.name);
+
     printf("Введите фамилию абонента: ");
-    scanf("%s", new_abonent.second_name);
-    printf("Введите телефон абонента: ");
-    scanf("%s", new_abonent.tel);
-    
-    (*directory)[*size - 1] = new_abonent;
-    
-    printf("Абонент добавлен.\n");
+    scanf("%s", newAbonent.second_name);
+
+    printf("Введите номер телефона абонента: ");
+    scanf("%s", newAbonent.tel);
+
+    *abonents = (struct abonent*)realloc(*abonents, (*count + 1) * sizeof(struct abonent));
+    (*abonents)[*count] = newAbonent;
+    (*count)++;
+
+    printf("Абонент успешно добавлен.\n");
 }
 
-void remove_abonent(struct abonent** directory, int* size) {
-    if (*size == 0) {
-        printf("Справочник пуст.\n");
+// Функция для удаления абонента из справочника
+void deleteAbonent(struct abonent **abonents, int *count) {
+    if (*count == 0) {
+        printf("Справочник пуст. Нет абонентов для удаления.\n");
         return;
     }
-    
-    char name[10];
-    printf("Введите имя абонента, которого нужно удалить: ");
-    scanf("%s", name);
-    
-    int found = 0;
-    for (int i = 0; i < *size; i++) {
-        if (strcmp((*directory)[i].name, name) == 0) {
-            // Перемещаем элемент в конец массива
-            (*directory)[i] = (*directory)[*size - 1];
-            
-            *size -= 1;
-            *directory = static_cast<abonent*>(realloc(*directory, (*size) * sizeof(struct abonent)));
-            
-            found = 1;
-            printf("Абонент удален.\n");
+
+    char nameToDelete[NAME_LENGTH];
+
+    printf("Введите имя абонента, которого хотите удалить: ");
+    scanf("%s", nameToDelete);
+
+    int i;
+    int deleted = 0;
+
+    for (i = 0; i < *count; i++) {
+        if (strcmp((*abonents)[i].name, nameToDelete) == 0) {
+            memmove(*abonents + i, *abonents + i + 1, (*count - i - 1) * sizeof(struct abonent));
+            *abonents = (struct abonent*)realloc(*abonents, (*count - 1) * sizeof(struct abonent));
+            (*count)--;
+            deleted = 1;
             break;
         }
     }
-    
-    if (!found) {
-        printf("Абонент не найден.\n");
+
+    if (deleted) {
+        printf("Абонент успешно удален.\n");
+    } else {
+        printf("Абонент с таким именем не найден.\n");
     }
 }
 
-void search_abonent(struct abonent* directory, int size) {
-    if (size == 0) {
-        printf("Справочник пуст.\n");
+// Функция для поиска абонента по имени
+void searchAbonent(struct abonent *abonents, int count) {
+    if (count == 0) {
+        printf("Справочник пуст. Нет абонентов для поиска.\n");
         return;
     }
-    
-    char name[10];
+
+    char nameToSearch[NAME_LENGTH];
+
     printf("Введите имя абонента для поиска: ");
-    scanf("%s", name);
-    
+    scanf("%s", nameToSearch);
+
+    int i;
     int found = 0;
-    for (int i = 0; i < size; i++) {
-        if (strcmp(directory[i].name, name) == 0) {
-            printf("Имя: %s\n", directory[i].name);
-            printf("Фамилия: %s\n", directory[i].second_name);
-            printf("Телефон: %s\n", directory[i].tel);
-            
+
+    printf("Результаты поиска:\n");
+
+    for (i = 0; i < count; i++) {
+        if (strcmp(abonents[i].name, nameToSearch) == 0) {
+            printf("Имя: %s\n", abonents[i].name);
+            printf("Фамилия: %s\n", abonents[i].second_name);
+            printf("Номер телефона: %s\n", abonents[i].tel);
             found = 1;
         }
     }
-    
+
     if (!found) {
-        printf("Абонент не найден.\n");
+        printf("Абонент с таким именем не найден.\n");
     }
 }
 
-void print_directory(struct abonent* directory, int size) {
-    if (size == 0) {
-        printf("Справочник пуст.\n");
+// Функция для вывода всех записей
+void printAllAbonents(struct abonent *abonents, int count) {
+    if (count == 0) {
+        printf("Справочник пуст. Нет абонентов для вывода.\n");
         return;
     }
-    
-    for (int i = 0; i < size; i++) {
-        printf("Абонент %d:\n", i + 1);
-        printf("Имя: %s\n", directory[i].name);
-        printf("Фамилия: %s\n", directory[i].second_name);
-        printf("Телефон: %s\n", directory[i].tel);
-        printf("\n");
+
+    int i;
+
+    printf("Список всех абонентов:\n");
+
+    for (i = 0; i < count; i++) {
+        printf("Имя: %s\n", abonents[i].name);
+        printf("Фамилия: %s\n", abonents[i].second_name);
+        printf("Номер телефона: %s\n", abonents[i].tel);
     }
 }
 
 int main() {
-    struct abonent* directory = NULL;
-    int size = 0;
+    struct abonent *abonents = NULL;
+    int count = 0;
+
     int choice;
-    
+
     do {
+        printf("\nМеню:\n");
         printf("1) Добавить абонента\n");
         printf("2) Удалить абонента\n");
         printf("3) Поиск абонентов по имени\n");
         printf("4) Вывод всех записей\n");
         printf("5) Выход\n");
-        printf("Выберите действие: ");
+        printf("Выберите пункт меню: ");
         scanf("%d", &choice);
-        
+
         switch (choice) {
             case 1:
-                add_abonent(&directory, &size);
+                addAbonent(&abonents, &count);
                 break;
             case 2:
-                remove_abonent(&directory, &size);
+                deleteAbonent(&abonents, &count);
                 break;
             case 3:
-                search_abonent(directory, size);
+                searchAbonent(abonents, count);
                 break;
             case 4:
-                print_directory(directory, size);
+                printAllAbonents(abonents, count);
                 break;
             case 5:
-                printf("Выход.\n");
+                free(abonents);
+                printf("Программа завершена.\n");
                 break;
             default:
-                printf("Некорректный ввод.\n");
+                printf("Неверный выбор. Пожалуйста, выберите пункт меню от 1 до 5.\n");
                 break;
         }
-        
-        printf("\n");
     } while (choice != 5);
-    
-    free(directory);
-    
+
     return 0;
 }
